787b977fa77ce09b6beb5e085e8179aedc45db02
Jaydin Madore <92885447+JaydinM@users.noreply.github.com>
2024-04-18 10:54:41 -0400

    Initial commit

---
+++ com/example/Factorial.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Jaydin Madore
+ * @version 1.0
+ * @since 2024-04-18
+ */
+
+// Factorial class
+public final class Factorial {
+
+    /** Private constructor to prevent instantiation. */
+    private Factorial() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
