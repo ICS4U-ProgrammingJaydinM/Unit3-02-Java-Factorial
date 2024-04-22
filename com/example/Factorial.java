645511ebdf8df9f7f2bc0129e14f912cf243d40a
Jaydin Madore <JaydinMadore@stu.ocsb.ca>
2024-04-22 14:22:00 +0000

    Factorial

--- com/example/Factorial.java
+++ com/example/Factorial.java
@@ -1,6 +1,11 @@
 
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 /**
  * Change me.
  *
@@ -17,12 +22,42 @@ public final class Factorial {
         throw new UnsupportedOperationException("Cannot instantiate");
     }
 
+
     /**
      * This is the main method.
      *
      * @param args Unused
      */
     public static void main(final String[] args) {
-        System.out.println("Hello, world");
+        // List of file paths to output to.
+        String outputFilePath = "output.txt";
+        // List of file paths to read from.
+        String readFilePath = "input.txt";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(readFilePath));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                int inputNumber = Integer.parseInt(line.trim());
+                //This calculates factorial
+                int result = factorialCul(inputNumber);
+                //This writes output to output.txt
+                writer.write(inputNumber + "! = " + result + "\n");
+            }
+            reader.close();
+            writer.close();
+
+        } catch (IOException e) {
+            System.err.println("Error reading/writing files: " + e.getMessage());
+        }
     }
+
+    private static int factorialCul (int n) {
+        //The base case for the recursion function
+        if (n == 1)
+        //This wll return 1    
+        return 1;
+        else
+            return n * factorialCul(n - 1);
+        }
 }
